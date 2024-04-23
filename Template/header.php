75f892326deca8c7277096b28168187ba0e3e6da
KrakenZoul <nicolasjavier0426@gmail.com>
2024-04-23 15:28:33 -0500

    primer commit

---
+++ Template/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <title>Biblioteca</title>
+</head>
+
+<body>
+  <div>
+    <ul>
+      <li><a href="../index.php">Inicio</a></li>
+      <li><a href="#news">Libros</a></li>
+      <li><a href="#contact">Prestamos</a></li>
+      <li><a href="#about">Busqueda de Libros</a></li>
+      <li><a href="#about">Libros En Prestamo</a></li>
+      <li><a href="#about">Entrega Libros</a></li>
+    </ul>
+    
+  </div>
