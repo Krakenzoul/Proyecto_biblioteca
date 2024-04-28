2f6fd0d9130aed987e1753d4b9fab0cb61408aa7
KrakenZoul <nicolasjavier0426@gmail.com>
2024-04-27 19:46:33 -0500

    2

--- Template/header.php
+++ Template/header.php
@@ -7,7 +7,7 @@ $url_base="http://localhost:3000";
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../style.css">
+  <link rel="stylesheet" href="../../style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <title>Biblioteca</title>
@@ -18,9 +18,9 @@ $url_base="http://localhost:3000";
     <ul>
       <li><a href="<?php echo $url_base;?>/index.php">Inicio</a></li>
       <li><a href="<?php echo $url_base;?>/Back/Libros/">Libros</a></li>
-      <li><a href="/Back/Buscar Libro/">Busqueda de Libros</a></li>
-      <li><a href="#contact">Prestamos</a></li>
-      <li><a href="<?php echo $url_base;?>/Back/tipo/">Tipo</a></li>
+      <li><a href="<?php echo $url_base;?>/Back/Buscar Libro/">Busqueda de Libros</a></li>
+      <li><a href="<?php echo $url_base;?>/Back/Prestamo">Prestamos</a></li>
+
     </ul>
     
   </div>
