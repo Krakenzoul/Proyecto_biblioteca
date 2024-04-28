3d8b4339ec612d2b0979ea57d5159dc5a57593c1
KrakenZoul <nicolasjavier0426@gmail.com>
2024-04-27 19:36:29 -0500

    2

--- Back/tipo/index.php
+++ Back/tipo/index.php
@@ -1,8 +1,15 @@
 <?php
 include ("Template/header.php");
 ?>
-<?php echo "pinga"; ?> 
+<div class="contenedor-index">
+    <div class="div-index1">
+        <p class="text-index">Bienvenido a gestor de la biblioteca</p>
+    </div>
+    <div class="div-index2">
+        <img src="imagen/Proyecto Biblioteca - Página 1.jpeg" height="500px" alt="buho_logo">
+    </div>
 
+</div>
 <?php
-include ("Template/footer.php");
+include ("template/footer.php");
 ?>
