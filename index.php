7cd9f3653e53182b4cb479a364b890c434088f9b
KrakenZoul <nicolasjavier0426@gmail.com>
2024-04-27 19:47:05 -0500

    2#	modified:   index.php

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
 include ("template/header.php");
 ?>
+<div class="contenedor-index">
+    <div class="div-index1">
+        <p class="text-index">Bienvenido a gestor de la biblioteca</p>
+    </div>
+    <div class="div-index2">
+        <img src="imagen/Proyecto Biblioteca - Página 1.jpeg" height="500px" alt="buho_logo">
+    </div>
 
+</div>
 <?php
 include ("template/footer.php");
 ?>
