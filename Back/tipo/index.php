37a792d326880a172731b1f90b0667c4af99e5bd
KrakenZoul <nicolasjavier0426@gmail.com>
2024-04-27 19:35:08 -0500

    2

--- Back/tipo/index.php
+++ Back/tipo/index.php
@@ -1,8 +1,8 @@
 <?php
-include ("/Template/header.php");
+include ("Template/header.php");
 ?>
 <?php echo "pinga"; ?> 
 
 <?php
-include ("/Template/footer.php");
+include ("Template/footer.php");
 ?>
