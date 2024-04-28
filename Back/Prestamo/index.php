03b26d74d4126c466d62022a42f8d8eab0d03010
KrakenZoul <nicolasjavier0426@gmail.com>
2024-04-27 20:07:27 -0500

    12

--- Back/Prestamo/index.php
+++ Back/Prestamo/index.php
@@ -2,7 +2,7 @@
 Buscar libro
 
 <?php 
-echo "<div style='border: 1px solid black; margin: 10px;'>Contenido del div con borde";
+echo "<div style='border: 1px solid black; margin: 100px;'>Contenido del div con borde";
 echo "</div>";
 ?>
 <?php include("../../Template/footer.php"); ?>
