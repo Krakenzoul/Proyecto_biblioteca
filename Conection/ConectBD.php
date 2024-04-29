25eababcb5767feea5053822673e0dddad97fd8b
KrakenZoul <nicolasjavier0426@gmail.com>
2024-04-28 22:26:08 -0500

    w

--- Conection/ConectBD.php
+++ Conection/ConectBD.php
@@ -1,12 +1,13 @@
 <?php 
     $server="127.0.0.1";
-    $data_base_name="p_biblioteca";
+    $data_base_name="bd_biblioteca";
     $user="root";
     $password="0426";
 
     try {
-        $conect= new PDO("mysql:host=$server;dbname=$data_base_name",$user,$password);
-    } catch (Exception $ex) {
-        echo $ex->getMessage();
-    }
+        $conexionpdo = new PDO("mysql:host=$server;dbname=$data_base_name",$user,$password);
+        }
+        catch (PDOException $e){
+        echo $e->getMessage();
+        }
 ?>
